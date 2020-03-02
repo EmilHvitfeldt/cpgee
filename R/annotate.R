195d7b538e1a062955104d48a5ff9358fb728657
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2020-03-02 15:40:49 -0800

    adding more annotating functions

--- R/annotate.R
+++ R/annotate.R
@@ -1,11 +1,11 @@
-#' Calculate distribution of cpg over chromosomes
+#' Annotate cpgs with chromosome information
 #'
 #' @param data A data.frame.
 #' @param var variable name containing the cpgs.
 #' @param array_type Type of array annotation data to use. Must be one of
 #' "450K" or "EPIC", defaults to "450K".
 #'
-#' @return [tibble][tibble::tibble-package] with two columns "chr" and "n".
+#' @return [tibble][tibble::tibble-package] with two columns.
 #' @export
 #'
 #' @examples
@@ -18,3 +18,72 @@ cpg_annotate_chromosome <- function(data, var, array_type = c("450K", "EPIC")) {
   chr <- factor(chr, paste0("chr", c(1:22, "X", "Y")))
   dplyr::bind_cols(data, tibble::tibble("chr" := .env$chr))
 }
+
+#' Annotate cpg with predefined regions
+#'
+#' @inheritParams cpg_annotate_chromosome
+#' @param region character, region to annotate according to. Must be one of
+#' "relation_to_island", "1stExon",  "3UTR", "5UTR", "Body", "TSS1500" or
+#' "TSS200". Defaults to "relation_to_island".
+#'
+#' @return [tibble][tibble::tibble-package] with two columns.
+#' @export
+#'
+#' @examples
+#' df_cpg <- data.frame(cpg = sample_cpgs)
+#' cpg_annotate_region(df_cpg, cpg)
+#' cpg_annotate_region(df_cpg, cpg, "1stExon")
+#' cpg_annotate_region(df_cpg, cpg, "3UTR")
+#' cpg_annotate_region(df_cpg, cpg, "5UTR")
+#' cpg_annotate_region(df_cpg, cpg, "Body")
+#' cpg_annotate_region(df_cpg, cpg, "TSS1500")
+#' cpg_annotate_region(df_cpg, cpg, "TSS200")
+cpg_annotate_region <- function(data, var,
+                                region = c("relation_to_island", "1stExon",
+                                         "3UTR", "5UTR", "Body", "TSS1500",
+                                         "TSS200"),
+                                array_type = c("450K", "EPIC")) {
+
+  region <- rlang::arg_match(region)
+
+  if (region == "relation_to_island") {
+    locations <- fetch(array_type, "island")
+    loc_var <- "Relation_to_Island"
+  }
+  if (region == "1stExon") {
+    locations <- fetch(array_type, "other")
+    locations[["gene_1stExon"]] <- grepl("1stExon", locations$UCSC_RefGene_Group)
+    loc_var <- "gene_1stExon"
+  }
+  if (region == "3UTR") {
+    locations <- fetch(array_type, "other")
+    locations[["gene_3utr"]] <- grepl("3'UTR", locations$UCSC_RefGene_Group)
+    loc_var <- "gene_3utr"
+  }
+  if (region == "5UTR") {
+    locations <- fetch(array_type, "other")
+    locations[["gene_5utr"]] <- grepl("5'UTR", locations$UCSC_RefGene_Group)
+    loc_var <- "gene_5utr"
+  }
+  if (region == "Body") {
+    locations <- fetch(array_type, "other")
+    locations[["gene_body"]] <- grepl("Body", locations$UCSC_RefGene_Group)
+    loc_var <- "gene_body"
+  }
+  if (region == "TSS1500") {
+    locations <- fetch(array_type, "other")
+    locations[["gene_TSS1500"]] <- grepl("TSS1500", locations$UCSC_RefGene_Group)
+    loc_var <- "gene_TSS1500"
+  }
+  if (region == "TSS200") {
+    locations <- fetch(array_type, "other")
+    locations[["gene_TSS200"]] <- grepl("TSS200", locations$UCSC_RefGene_Group)
+    loc_var <- "gene_TSS200"
+  }
+
+  chr <- locations[rownames(locations) %in% dplyr::pull(data, {{var}}), loc_var]
+  res <- dplyr::bind_cols(data, tibble::tibble({{loc_var}} := .env$chr))
+  dplyr::as_tibble(res)
+}
+
+
--- R/fetch.R
+++ R/fetch.R
@@ -9,4 +9,22 @@ fetch <- function(array_type = c("450K", "EPIC"), table) {
       return(IlluminaHumanMethylationEPICanno.ilm10b4.hg19::Locations)
     }
   }
+
+  if (table == "island") {
+    if (array_type == "450K") {
+      return(IlluminaHumanMethylation450kanno.ilmn12.hg19::Islands.UCSC)
+    }
+    if (array_type == "EPIC") {
+      return(IlluminaHumanMethylationEPICanno.ilm10b4.hg19::Islands.UCSC)
+    }
+  }
+  if (table == "other") {
+    if (array_type == "450K") {
+      return(IlluminaHumanMethylation450kanno.ilmn12.hg19::Other)
+    }
+    if (array_type == "EPIC") {
+      return(IlluminaHumanMethylationEPICanno.ilm10b4.hg19::Other)
+    }
+  }
+
 }
